a5ba241fc180de9417dc86d438d61f2266868223
AlanMWatson <alphawhiskey82@yahoo.com>
2021-11-19 16:46:40 -0500

    Start work on zarr conversion script

---
+++ bil_api/development/convert_fMOST_H5_v001.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 11:26:46 2021
+
+@author: alpha
+"""
+
---
+++ bil_api/development/zarrNestedFormatConverter.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 12:18:24 2021
+
+@author: alpha
+"""
+
+import os, glob, zarr
+import dask.array as da
+from dask.delayed import delayed
+from skimage import io
+import itertools
+
+
+'''
+Expects data to be ordered as:
+    T[0-9]
+        CH[0-9]
+            image0001.tif
+            image0002.tif
+            ...
+'''
+
+dataToConvert = r"H:\globus\pitt\bil"
+scale = (1.0, 0.35, 0.35)  ## (z,y,x) arbitraty units but is probably microns
+
+outputLocation = os.path.join(dataToConvert,'fMOST.zarr')
+
+print('Finding Timepoints')
+timeDirs = sorted(glob.glob(os.path.join(dataToConvert,'T[0-9]')))
+
+print('Finding Channels')
+if len(timeDirs) > 0: 
+    channelDirs = [sorted(glob.glob(os.path.join(x,'CH[0-9]'))) for x in timeDirs]
+else:
+    channelDirs = sorted(glob.glob(os.path.join(dataToConvert,'CH[0-9]')))
+
+print('Finding Images')
+if len(channelDirs) > 0: 
+    images = [sorted(glob.glob(os.path.join(x,'*.tif'))) for x in channelDirs]
+else:
+    images = sorted(glob.glob(os.path.join(dataToConvert,'*.tif')))
+
+print('Loading a sample image')
+sampleImage = io.imread(images[0][0])
+
+print('Forming lazy dask array')
+data = []
+for ch in images:
+    data.append(
+        [delayed(io.imread)(x) for x in ch]
+        )
+
+    data[-1] = [
+        da.from_delayed(x,
+        shape=(sampleImage.shape[0],sampleImage.shape[1]), 
+        dtype=sampleImage.dtype) for x in data[-1]
+        ]
+
+stack = da.stack(data)
+
+while len(stack.shape) < 5:
+    stack = stack[None,...]
+
+
+'''
+Assumptions
+
+After the previous code, 'stack' will be a multi-dimentional lazy dask array
+dims=(t,c,z,y,x).  Less than 5 dims will remove the left-most dim.
+
+for example stack.shape == (2, 11464, 30801, 20821) is assumed to be (c,z,y,x) with no time dim
+
+
+multi resolution zarr arrays will be stored in a single directory:
+Resolution, time and channel will have separate dirs.
+
+    r[0-9][0-9]
+        t[0-9][0-9]
+            c[0-9][0-9]
+                'data' = 3D_zarr_array_NestedDirectoryStore
+            
+'''
+
+################################################################################
+'''
+Resolution series should converge towards 3D isotropic but always with 2x reductions 
+in each dimension and never going beyond iostropic.  
+this requires knowing the origional z,y,x scale
+
+Fit to exactly iostropic with reductions in resolution no greater than 2x
+
+RLevel:  Resolution            Pixel Shape         Chunks
+
+resolutions = {
+ 0: ((1.0, 0.35, 0.35), (11464, 30801, 20821), (1, 1024, 1024)),
+ 1: ((1.0, 0.7, 0.7), (11464, 15400, 10410), (1, 1024, 1024)),
+ 2: ((1.0, 1.0, 1.0), (11464, 10779, 7286), (2, 512, 512)),
+ 3: ((2.0, 2.0, 2.0), (5732, 5389, 3643), (2, 512, 512)),
+ 4: ((4.0, 4.0, 4.0), (2866, 2694, 1821), (4, 256, 256)),
+ 5: ((8.0, 8.0, 8.0), (1433, 1347, 910), (4, 256, 256)),
+ 6: ((16.0, 16.0, 16.0), (716, 673, 455), (8, 128, 128)),
+ 7: ((32.0, 32.0, 32.0), (358, 336, 227), (8, 128, 128)),
+ 8: ((64.0, 64.0, 64.0), (179, 168, 113), (16, 64, 64)),
+ 9: ((128.0, 128.0, 128.0), (89, 84, 56), (16, 64, 64)),
+ 10: ((256.0, 256.0, 256.0), (44, 42, 28), (32, 32, 32))
+ }
+
+Reworked code to converge on isotropic and then continue 2x decrase in resolition
+'''
+
+
+### Determine number of resolution series
+maxChunkSize = (1,1024,1024)
+minChunkSize = (32,64,64)
+minPixelShape = 16
+currentShape = stack.shape[-3::]
+currentResolution = scale
+currentChunks = maxChunkSize
+
+resolutions = {0:(currentResolution,currentShape, currentChunks)} # Key is resolution number, value = (resolution, pixels, chunks)
+
+# level = 1
+# while all([x//2 > minPixelShape for x in currentShape]):
+    
+#     if currentResolution[1]*2 <= currentResolution[0] and currentResolution[2]*2 <= currentResolution[0]:
+#         currentResolution = (currentResolution[0],currentResolution[1]*2,currentResolution[2]*2)
+#         currentShape = (currentShape[0],currentShape[1]//2,currentShape[2]//2)
+#         resolutions[level] = (
+#             currentResolution,
+#             currentShape,
+#             currentChunks
+#             )
+#     else:
+#         resolutions[level] = (
+#             tuple([x*2 for x in currentResolution]),
+#             tuple([x//2 for x in currentShape]),
+#             currentChunks
+#             )
+    
+#         currentResolution = resolutions[level][0]
+#         currentShape = resolutions[level][1]
+    
+#     level += 1
+    
+#     if level%2 == 0:
+#         currentChunks = (currentChunks[0]*2,currentChunks[1]//2,currentChunks[2]//2)
+    
+
+
+
+level = 1
+while all([x//2 > minPixelShape for x in currentShape]):
+    
+    if len(set(currentResolution)) == 1:
+        resolutions[level] = (
+            tuple([x*2 for x in currentResolution]),
+            tuple([x//2 for x in currentShape]),
+            currentChunks
+            )
+    
+        currentResolution = resolutions[level][0]
+        currentShape = resolutions[level][1]
+    
+    elif len(set(currentResolution)) > 1:
+        
+        resSet = list(set(currentResolution))
+        newResolution = []
+        newPixels = []
+        
+        changeFactor = 1
+        for rr,ss in zip(currentResolution,currentShape):
+            if rr < resSet[-1]:
+                changeFactor = resSet[-1]/rr if resSet[-1]/rr <=2 else 2
+            else:
+                changeFactor = 1
+            
+            newResolution.append(rr*changeFactor)
+            newPixels.append(ss//changeFactor)
+        
+        currentResolution = tuple(newResolution)
+        currentShape = tuple([int(x) for x in newPixels])
+        
+        resolutions[level] = (
+            currentResolution,
+            currentShape,
+            currentChunks
+            )
+    
+    level += 1
+    # Set chunk shape for next resolution level
+    if level%2 == 0:
+        currentChunks = (currentChunks[0]*2,currentChunks[1]//2,currentChunks[2]//2)
+    
+    
+    
+
+
+
+###  Setup zarr layout
+os.makedirs(outputLocation,exist_ok=True)
+
+def locationGenerator(r,t,c):
+    return '{}/{}/{}'.format(str(r).zfill(2),
+                            str(t).zfill(2),
+                            str(c).zfill(2)
+                            )
+
+for r,t,c, in itertools.product(
+        resolutions,
+        range(stack.shape[0]),
+        range(stack.shape[1])
+        ):
+    
+    store = zarr.NestedDirectoryStore(outputLocation)
+    root = zarr.group(store=store, overwrite=True)
+    foo = root.create_group(locationGenerator(r,t,c))
+    foo.zeros('data', shape=resolutions[r][1],chunks)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
