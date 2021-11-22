60f546c98a55d040cdce975064ca8ea3fd6d766c
AlanMWatson <alphawhiskey82@yahoo.com>
2021-11-22 12:35:15 -0500

    Latest

--- bil_api/development/zarrNestedFormatConverter.py
+++ bil_api/development/zarrNestedFormatConverter.py
@@ -10,7 +10,7 @@ import dask.array as da
 from dask.delayed import delayed
 from skimage import io
 from skimage.filters import gaussian
-from skimage import img_as_float32, img_as_uint16, img_as_ubyte
+from skimage import img_as_float32, img_as_uint, img_as_ubyte
 import itertools
 
 
@@ -91,7 +91,7 @@ Fit to exactly iostropic with reductions in resolution no greater than 2x
 
 Chunks will change every 2 levels. (z*2,y//2,x//2)
 
-RLevel:  Resolution            Pixel Shape         Chunks
+RLevel:  Resolution            Pixel Shape         Chunks           Downsample Factor
 
 resolutions = {
  0: ((1.0, 0.35, 0.35), (11464, 30801, 20821), (1, 1024, 1024)),
@@ -118,7 +118,7 @@ currentShape = stack.shape[-3::]
 currentResolution = scale
 currentChunks = maxChunkSize
 
-resolutions = {0:(currentResolution,currentShape, currentChunks)} # Key is resolution number, value = (resolution, pixels, chunks)
+resolutions = {0:(currentResolution,currentShape, currentChunks, (1,1,1))} # Key is resolution number, value = (resolution, pixels, chunks)
 
 print('Calculating Resolution Levels')
 level = 1
@@ -128,7 +128,8 @@ while all([x//2 > minPixelShape for x in currentShape]):
         resolutions[level] = (
             tuple([x*2 for x in currentResolution]),
             tuple([x//2 for x in currentShape]),
-            currentChunks
+            currentChunks,
+            (2,2,2)
             )
     
         currentResolution = resolutions[level][0]
@@ -136,17 +137,19 @@ while all([x//2 > minPixelShape for x in currentShape]):
     
     elif len(set(currentResolution)) > 1:
         
-        resSet = list(set(currentResolution))
+        resSet = sorted(list(set(currentResolution)))
         newResolution = []
         newPixels = []
         
         changeFactor = 1
+        DSF = []
         for rr,ss in zip(currentResolution,currentShape):
             if rr < resSet[-1]:
                 changeFactor = resSet[-1]/rr if resSet[-1]/rr <=2 else 2
             else:
                 changeFactor = 1
             
+            DSF.append(changeFactor)
             newResolution.append(rr*changeFactor)
             newPixels.append(ss//changeFactor)
         
@@ -156,7 +159,8 @@ while all([x//2 > minPixelShape for x in currentShape]):
         resolutions[level] = (
             currentResolution,
             currentShape,
-            currentChunks
+            currentChunks,
+            tuple(DSF)
             )
     
     level += 1
@@ -190,21 +194,21 @@ for r in resolutions:
     del root
     del store
 
-## Mount dask arrays from zarr
-zarr_arrays = {}
-for r in resolutions:
-    print('Opening zarr arrays {}'.format(str(r).zfill(2)))
-    location=os.path.join(outputLocation,str(r).zfill(2))
-    zarr_arrays[r] = zarr.open(
-        zarr.NestedDirectoryStore(location),
-        mode='a'
-        )
+# ## Mount dask arrays from zarr
+# zarr_arrays = {}
+# for r in resolutions:
+#     print('Opening zarr arrays {}'.format(str(r).zfill(2)))
+#     location=os.path.join(outputLocation,str(r).zfill(2))
+#     zarr_arrays[r] = zarr.open(
+#         zarr.NestedDirectoryStore(location),
+#         mode='a'
+#         )
 
 
-## For writing arrays we will split them by color
-toWrite = []
-for dd in range(stack.shape[1]):
-    toWrite.append(stack[:,[dd]])
+# ## For writing arrays we will split them by color
+# toWrite = []
+# for dd in range(stack.shape[1]):
+#     toWrite.append(stack[:,[dd]])
     
 
 def smooth(image, sigma=(0,0,0)):
@@ -217,7 +221,7 @@ def smooth(image, sigma=(0,0,0)):
     workingImg = gaussian(workingImg,sigma)
     
     if dtype == 'uint16':
-        workingImg = img_as_uint16(workingImg)
+        workingImg = img_as_uint(workingImg)
     elif dtype == 'uint8':
         workingImg = img_as_ubyte(workingImg)
     
@@ -232,13 +236,15 @@ for r in resolutions:
     if r == 0:
         downSample[r] = stack.rechunk(chunks=(1,
                                               1,
-                                              resolutions[r][2][0],
+                                              10,
+                                              # resolutions[r][2][0],
                                               resolutions[r][2][1],
                                               resolutions[r][2][2]))
     else:
         blured = downSample[r-1]
         
-        downSampleFactor = tuple([x/y for x,y in zip(resolutions[r-1][1],resolutions[r][1])])
+        downSampleFactor = resolutions[r][3]
+        # downSampleFactor = tuple([x/y for x,y in zip(resolutions[r-1][1],resolutions[r][1])])
         sigma = tuple([(x - 1) / 2 for x in downSampleFactor])
         depth = tuple([math.ceil(2*x) for x in sigma])
         blured = blured.map_overlap(smooth,
@@ -247,13 +253,24 @@ for r in resolutions:
                                     boundry='reflect')
         
         ## HOW TO DEAL WITH NON INT DOWNSAMPLES
-        subSampled = blured[1::2,]
+        z = list(np.round(np.linspace(0, resolutions[r-1][1][0] - 1, resolutions[r][1][0])).astype(int))
+        y = list(np.round(np.linspace(0, resolutions[r-1][1][1] - 1, resolutions[r][1][1])).astype(int))
+        x = list(np.round(np.linspace(0, resolutions[r-1][1][2] - 1, resolutions[r][1][2])).astype(int))
+
+        downSample[r] = blured[z,y,x]
 
 
 
 
-
-
+## Create delayed da.to_zarr in a list that can be used to call compute
+zarr_arrays = []
+for r in resolutions:
+    print('Create delayed da.to_zarr resolution {}'.format(str(r).zfill(2)))
+    location=os.path.join(outputLocation,str(r).zfill(2))
+    zarr_arrays.append(
+        downSample[r],
+        compute=False
+        )
 
 
 
