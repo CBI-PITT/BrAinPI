ce00fa7fb4e598cdda111b6ab27b292906d862fb
Alan M Watson <alan.watson@pitt.edu>
2022-03-29 18:24:06 -0400

    need to fix auth users can manually browse to un authorized locations

--- bil_api/fs_browse.py
+++ bil_api/fs_browse.py
@@ -62,6 +62,7 @@ def initiate_browseable(app):
         else:
             
             try:
+                print('Line 65')
                 # Determine what directories that anon users are allowed to browse 
                 # and kick them if the path is not valid
                 path_map = {}
@@ -78,6 +79,7 @@ def initiate_browseable(app):
                 # Determine what paths authenticated users may browse
                 # and kick them if the path is not valid
                 if current_user.is_authenticated:
+                    print('Line 82')
                     for ii in settings['dir_auth']:
                         path_map[ii] = settings['dir_auth'][ii]
                 print(path_map)
@@ -101,19 +103,28 @@ def initiate_browseable(app):
                 
                 # If the user is anonymous, then there is no additional filtering to do
                 if current_user.is_authenticated == False:
+                    print('Line 106')
                     pass
                 
-                else:
+                elif current_user.is_authenticated:
+                    print('Line 110')
                     
-                    # Designed to directly filter 1 level above paths designated in settins.ini
-                    if len(html_path) == 2 and \
-                        settings.getboolean('auth', 'restrict_paths_to_matched_username') and \
-                            current_user.is_authenticated:
-                        
-                        groups = get_config('groups.ini',allow_no_value=True)
-                        
-                        if current_user.id.lower() not in [x.lower() for x in groups['all']]:
+                    # Grab groups from groups.ini file
+                    # 'All' group is assumed to have NO restrctions, so all filters are bypassed for this group
+                    groups = get_config('groups.ini',allow_no_value=True)
+                    
+                    if current_user.id.lower() in [x.lower() for x in groups['all']]:
+                        print('Line 117')
+                        pass
+                    
+                    else:
+                        print('Line 121')
+                        # All paths +1 above those listed in 'setting.ini' needs to be filtered for 
+                        # username_or_group affiliation.  (ie base/path_in_settings.ini/username_or_group/anything_else/doesnt_matter)
+                        if settings.getboolean('auth', 'restrict_paths_to_matched_username'):
+                            print('Line 125')
                             
+                            ## to_view will collect all OK paths for viewing
                             to_view = []
                             
                             # Retain all anon paths
@@ -121,43 +132,60 @@ def initiate_browseable(app):
                                 tmp = [x for x in to_browse if settings['dir_anon'][ii] in x]
                                 to_view = to_view + tmp
                             
-                            # Keep only those paths with username
-                            to_view = to_view + [x for x in to_browse if '/' + current_user.id.lower() in x.lower()]
+                            # Keep only those paths from to_browse which contain '/username/' <-- may need to change this to filter at level html_path[2]
+                            print(current_user.id.lower())
+                            print(to_browse[0:10])
+                            to_view = to_view + [x for x in to_browse if '/' + current_user.id.lower() in x.lower() + '/']
                             print(to_view)
                             
-                            # Keep paths named for the groups a user belongs to
+                            # Keep only those paths from to_browse named for the groups a user belongs to
                             for ii in groups: # Group names
                                 for oo in groups[ii]: # Users in each group
                                     if current_user.id.lower() == oo.lower(): # Current user matches the user in the group
-                                        to_view = to_view + [x for x in to_browse if '/' + ii.lower() in x.lower()] # Retain group folder names
+                                        print('Line 143')
+                                        to_view = to_view + [x for x in to_browse if '/' + ii.lower() + '/' in x.lower() + '/'] # Retain group folder names '/group_name/'
                                     
+                            # Reset to_browse to be only paths which are included
                             to_browse = to_view
-                    
-                    
-                    ## Limit files that are seen to those in settings.ini 'file_types'
-                    groups = get_config('groups.ini',allow_no_value=True)
-                    if current_user.is_authenticated and current_user.id.lower() not in [x.lower() for x in groups['all']]:
+                        
+                        
+                        ## Limit files that are seen to those in settings.ini 'file_types'
                         if settings.getboolean('auth','restrict_files_to_listed_file_types'):
+                            
+                            ## to_view will collect all OK paths for viewing
                             to_view = []
+                            
+                            # First find all directories (these will not be filtered out)
                             dirs = [x for x in to_browse if os.path.isdir(x)]
+                            
+                            # Now find all files that match an allowed file type
+                            # First get a list of all files
+                            files = [ x for x in to_browse if os.path.isfile(x) ]
                             for ii in settings['file_types']:
-                                files = [ x for x in to_browse if (os.path.isfile(x) and settings['file_types'][ii] == os.path.splitext('a'+ x)[-1]) ]#<-- add 'a' to make split consistance for files starting with '.'
-                                to_view = to_view + files
+                                # Further limit to the file type being filtered
+                                current_files = [ x for x in files if settings['file_types'][ii] == os.path.splitext('a'+ x)[-1] ]#<-- add 'a' to make split consistent when files start with '.'
+                                to_view = to_view + current_files
                             to_view = dirs + to_view
+                            
+                            # Reset to_browse to be only paths which are included
                             to_browse = to_view
                     
                 
-                # Format path to align with the browser and disguise real paths
+                print(to_browse)
+                # Format paths to align with the browser representation and thus disguise real paths
                 to_browse = [x.replace(path_map[html_path[1]],'/' + html_path[1]) for x in to_browse]
+                
+                print(to_browse)
                 
                 # Sort for easy browsing
                 to_browse = sorted(to_browse)
                 
                 # Get path / file info to pass to html template
                 path = [base[:-1] + os.path.split(x)[0] for x in to_browse] if len(to_browse) > 0 else ['/']
-                files = [os.path.split(x)[1] for x in to_browse]
+                files = [os.path.split(x)[1] for x in to_browse] #if len(to_browse) > 0 else ['']
             
                 return render_template('vfs_bil.html', path=path, files=files)
+            
             except Exception:
                 flash('You must not be authorized to browse to path {}'.format(request.path))
                 return redirect(url_for('login'))
