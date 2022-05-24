474a6a291426c6c26b5b15215adde445f26460cf
Alan M Watson <alan.watson@pitt.edu>
2022-05-24 12:38:11 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 
 
 # https://github.com/pypa/setuptools_scm
-use_scm = {"write_to": "bil_api/_version.py"}
+use_scm = {"write_to": "BrAinPI/_version.py"}
 setup(use_scm_version=use_scm)
