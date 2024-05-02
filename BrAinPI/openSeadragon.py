91a3d1c3a3e3bdc8fc97c72312ef22a4c41b0ad9
collin2046 <collin9527@gmail.com>
2024-05-02 16:50:19 -0400

    change cache to execute on data,not the generated png imgs, add new endpoint view for loading page and gtag

--- BrAinPI/openSeadragon.py
+++ BrAinPI/openSeadragon.py
@@ -27,7 +27,7 @@
 # #   Overlap="1"
 # #   TileSize="{tile_size}"
 # # >
-# #   <Size 
+# #   <Size
 # #     Height="{height}"
 # #     Width="{width}"
 # #   />
@@ -65,7 +65,7 @@
 #     def openseadragon_entry(req_path):
 #         path_split, datapath = get_html_split_and_associated_file_path(config, request)
 #         print(path_split, datapath)
-        
+
 #         if utils.split_html(datapath)[-1].endswith(tuple(openseadragon_dtypes())):
 #             datapath = config.loadDataset(datapath)
 #             numpy_tif = config.opendata[datapath]
@@ -75,8 +75,8 @@
 #             return render_template("openseadragon_temp.html",height=int(numpy_tif.height), width = int(numpy_tif.width),tileSize = int(numpy_tif.tile_size[0]), parent_url= '/'.join(path_split), resolutions = numpy_tif.resolutions, value = numpy_tif.channels, z_stack= numpy_tif.z)
 #         elif utils.split_html(datapath)[-1].endswith('.png'):
 #             # print('end with jpeg')
-            
-#             # NumPy array 
+
+#             # NumPy array
 #             # to be optimized!!!
 #             datapath_split = datapath.split('/')
 #             datapath = config.loadDataset('/'+os.path.join(*datapath_split[:-6]))
@@ -126,62 +126,20 @@ import os
 from flask import (
     render_template,
     request,
-    send_file,
     redirect,
     jsonify,
-    make_response,
     Response,
 )
-import numpy as np
 from PIL import Image
 import io
 from flask_cors import cross_origin
-import tiff_loader
 
 
 def openseadragon_dtypes():
-    return [".tif",
-            ".tiff",
-            ".ome.tif",
-            ".ome.tiff",
-            ".ome-tif",
-            ".ome-tiff"
-            ]
+    return [".tif", ".tiff", ".ome.tif", ".ome.tiff", ".ome-tif", ".ome-tiff"]
 
 
 openSeadragonPath = "/op_seadragon/"
-# dzi_template = """<?xml version="1.0" encoding="UTF-8"?>
-# <Image xmlns="http://schemas.microsoft.com/deepzoom/2008"
-#   Format="{format}"
-#   Overlap="1"
-#   TileSize="{tile_size}"
-# >
-#   <Size 
-#     Height="{height}"
-#     Width="{width}"
-#   />
-# </Image>"""
-# def dzi_exist_check(numpy_tif):
-#     if not hasattr(numpy_tif, "dzi_file"):
-#         # print("NOOOO dzi")
-#         numpy_tif.dzi_file= dzi_generation(numpy_tif)
-#         return numpy_tif.dzi_file
-#     else:
-#         # print("has dzi")
-#         return numpy_tif.dzi_file
-# def dzi_generation(numpy_tif):
-#     height = numpy_tif.height
-#     width = numpy_tif.width
-#     tile_size = numpy_tif.tile_size
-#     image_format = numpy_tif.format
-
-#     # DZI XML template
-
-#     # Format the XML template with dynamic values
-#     dzi_xml = dzi_template.format(
-#         height=height, width=width, tile_size=tile_size, format=image_format
-#     )
-#     return dzi_xml
 
 
 def setup_openseadragon(app, config):
@@ -193,82 +151,101 @@ def setup_openseadragon(app, config):
 
     def openseadragon_entry(req_path):
         path_split, datapath = get_html_split_and_associated_file_path(config, request)
-        # print(path_split, datapath)
-        
+        print(path_split, datapath)
+
         if utils.split_html(datapath)[-1].endswith(tuple(openseadragon_dtypes())):
+            datapath_split = datapath.split("/")
+            file_name = datapath_split[-1]
+            view_path = request.path + '/view'
+            return render_template("loading.html",gtag=config.settings.get('GA4','gtag'),
+                                   redirect_url=view_path,
+                                   redirect_name='OpenSeadragon',
+                                   description=datapath,
+                                   file_name = file_name
+                                   )
             datapath = config.loadDataset(datapath)
             tif_obj = config.opendata[datapath]
-           
-            return render_template("openseadragon_temp.html",height=int(tif_obj.height), width = int(tif_obj.width),tileSize = int(tif_obj.tile_size[0]), parent_url= '/'.join(path_split), resolutions = tif_obj.series, t_point = tif_obj.t, value = tif_obj.channels, z_stack= tif_obj.z)
-        elif utils.split_html(datapath)[-1].endswith('.png'):
+
+            return render_template(
+                "openseadragon_temp.html",
+                height=int(tif_obj.height),
+                width=int(tif_obj.width),
+                tileSize=int(tif_obj.tile_size[0]),
+                host=config.settings.get("app", "url"),
+                parent_url="/".join(path_split),
+                resolutions=tif_obj.series,
+                t_point=tif_obj.t,
+                value=tif_obj.channels,
+                z_stack=tif_obj.z,
+            )
+        elif utils.split_html(datapath)[-1].endswith("view"):
+            path_split_list = list(path_split)
+            path_split_list.remove('view')
+            path_split_tuple = tuple(path_split_list)
+            datapath = datapath.replace('/view', '')
+            datapath = config.loadDataset(datapath)
+            tif_obj = config.opendata[datapath]
+            return render_template(
+                "openseadragon_temp.html",
+                height=int(tif_obj.height),
+                width=int(tif_obj.width),
+                tileSize=int(tif_obj.tile_size[0]),
+                host=config.settings.get("app", "url"),
+                parent_url="/".join(path_split_tuple),
+                resolutions=tif_obj.series,
+                t_point=tif_obj.t,
+                value=tif_obj.channels,
+                z_stack=tif_obj.z,
+            )
+       
+        elif utils.split_html(datapath)[-1].endswith(".png"):
             # print('end with png')
-            
-            datapath_split = datapath.split('/')
-            datapath = config.loadDataset('/'+os.path.join(*datapath_split[:-7]))
+
+            datapath_split = datapath.split("/")
+            # The actual path excluded the r-t-c-z-y-x parameters
+            datapath = config.loadDataset("/" + os.path.join(*datapath_split[:-7]))
             # print('datapath', datapath)
             tif_obj = config.opendata[datapath]
-            
+
             key = datapath_split[-7:-1]
             # return get_slice(tif_obj,key)
-            cache_key = f'opsd_{datapath}-{key[0]}-{key[1]}-{key[2]}-{key[3]}-{key[4]}--{key[5]}'
+            cache_key = f"opsd_{datapath}-{key[0]}-{key[1]}-{key[2]}-{key[3]}-{key[4]}--{key[5]}"
             slice = None
             if config.cache is not None:
                 # print("cache not none")
                 slice = config.cache.get(cache_key, default=None, retry=True)
-                if slice:
-                    print('return from cache')
+                if slice is not None:
+                    print("return from cache")
             if slice is None:
-                # retrieve from the zarr 
+                # retrieve from the zarr
                 slice = tif_obj[key]
+                print("return from disk")
+            pil_image = Image.fromarray(slice)
 
-                pil_image = Image.fromarray(slice)
+            # Create an in-memory byte stream to store the image data
+            image_stream = io.BytesIO()
 
-                # Create an in-memory byte stream to store the image data
-                image_stream = io.BytesIO()
+            # Save the PIL image as png to the in-memory byte stream
+            pil_image.save(image_stream, format="png")
 
-                # Save the PIL image as png to the in-memory byte stream
-                pil_image.save(image_stream, format='png')
+            # Seek to the beginning of the stream (important
+            image_stream.seek(0)
+            slice = image_stream
 
-                # Seek to the beginning of the stream (important
-                image_stream.seek(0)
-                slice = image_stream
-                print('return from disk')
-                if config.cache is not None:
-                    config.cache.set(cache_key, slice, expire=None, tag=datapath, retry=True)
-            return Response(slice, mimetype='image/png')
-            
+            # if config.cache is not None:
+            #     config.cache.set(cache_key, slice, expire=None, tag=datapath, retry=True)
+            return Response(slice, mimetype="image/png")
+
         else:
-            return 'No end point recognized!'
+            return "No end point recognized!"
 
-    openseadragon_entry = cross_origin(allow_headers=["Content-Type"])(openseadragon_entry)
-    openseadragon_entry = app.route(openSeadragonPath + "<path:req_path>")(openseadragon_entry)
+    openseadragon_entry = cross_origin(allow_headers=["Content-Type"])(
+        openseadragon_entry
+    )
+    openseadragon_entry = app.route(openSeadragonPath + "<path:req_path>")(
+        openseadragon_entry
+    )
+    # Not sure if it should be included or not
     openseadragon_entry = app.route(openSeadragonPath, defaults={"req_path": ""})(
         openseadragon_entry
     )
-    # def get_slice(tif_obj, key):
-    #     "r t c z y x (e)"
-    # #  # symmetric 0 --> 8
-    #     r = int(key[0])
-    #     c = int(key[1])
-    #     z = int(key[2])
-    #     y = int(key[3])
-    #     x = int(key[4])
-    #     tile_size = int(tif_obj.tile_size[0])
-    # #  return((numpy_tif.array[r][0,0,0]))
-    # #  print(numpy_tif.array[r][0,c,0,y*tile_size:(y+1)*tile_size,x*tile_size:(x+1)*tile_size].shape)
-    # #  return tif_obj.array[r][0,c,z,y*tile_size:(y+1)*tile_size,x*tile_size:(x+1)*tile_size]
-    #     numpy_array = np.random.randint(0, 255, size=(256, 256, 3), dtype=np.uint8)
-    #     slice = numpy_array
-    #     pil_image = Image.fromarray(slice)
-
-    #     # Create an in-memory byte stream to store the image data
-    #     image_stream = io.BytesIO()
-
-    #     # Save the PIL image as JPEG to the in-memory byte stream
-    #     pil_image.save(image_stream, format='png')
-
-    #     # Seek to the beginning of the stream (important)
-    #     image_stream.seek(0)
-    #     slice = image_stream
-    #     return Response(slice, mimetype='image/png')
-
