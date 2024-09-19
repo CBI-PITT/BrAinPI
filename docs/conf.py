32bf7089197e4e7497ce14637843e7a04bf31f8e
collin2046 <collin9527@gmail.com>
2024-09-19 17:21:06 -0400

    conf update

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../BrAinPI'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../BrAinPI'))
 # print(sys.path)
 project = 'BrainPi Document'
 copyright = '2024, Alan M Watson, Kelin He'
