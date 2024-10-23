56e4aee582ec70a3b9fdace2363cad5280e2a899
collin2046 <collin9527@gmail.com>
2024-10-23 16:51:49 -0400

    update docs config

--- docs/conf.py
+++ docs/conf.py
@@ -23,8 +23,8 @@ extensions = ["sphinx.ext.autodoc",
               "sphinx.ext.napoleon",
               "sphinx.ext.autosummary"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# templates_path = ['_templates']
+exclude_patterns = ['_build', '_templates', '_static','Thumbs.db', '.DS_Store']
 
 
 
@@ -32,5 +32,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
